a1c42e707bb4adf8e221c9720dc54e8153b0d3df
jelimo-charity <charityjelimo893@gmail.com>
2023-04-22 22:05:20 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const checkButton = document.getElementById("checkBtn").onclick= function(){
+    location.href="bookstore.html";
+};
+
+const connectButtons = document.getElementsByClassName("btn").onclick= function(){
+    location.href="connect.html";
+    
+};
